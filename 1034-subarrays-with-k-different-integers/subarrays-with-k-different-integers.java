ab70d7143b80e6b97b3b3a71d4b6bf6e8d5149fa
Mounika Madupoju <madupojumounika0@gmail.com>
2026-08-11 04:32:30 +0530

    Time: 49 ms (57.21%) | Memory: 50.4 MB (14.96%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -8,20 +8,16 @@ class Solution {
 
         int left = 0;
         int count = 0;
-        int distinct = 0;
+       
         for(int right = 0; right < nums.length; right++){
 
-            if(map.getOrDefault(nums[right], 0) == 0){
-                distinct++;
-            }
             map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
 
-            while(distinct > k){
+            while(map.size() > k){
                 map.put(nums[left], map.get(nums[left]) - 1);
 
                 if(map.get(nums[left]) == 0){
                     map.remove(nums[left]);
-                    distinct--;
                 }
                 left++;
             }
